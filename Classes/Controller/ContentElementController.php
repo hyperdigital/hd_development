c846894c680327d00d6b606613e9e9255415c474
Martin Pribyl <martin.pribyl@hyperdigital.de>
2024-04-09 10:23:32 +0200

    Enabled partials

--- Classes/Controller/ContentElementController.php
+++ Classes/Controller/ContentElementController.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 namespace Hyperdigital\HdDevelopment\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Configuration\ConfigurationManager;
 use TYPO3\CMS\Core\Resource\FileReference;
 use TYPO3\CMS\Core\Resource\FileRepository;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\PathUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
@@ -17,6 +21,7 @@ class ContentElementController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
 
     public function __construct(
         protected readonly FileRepository $fileRepository,
+    protected readonly ResourceFactory $resourceFactory,
     ) {
 
     }
@@ -32,7 +37,33 @@ class ContentElementController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
             if ($content) {
                 $view = GeneralUtility::makeInstance(StandaloneView::class);
                 $view->setTemplateSource($content);
-                $view->assignMultiple($this->settings['variables']);
+                if ($this->settings['variables']) {
+                    $view->assignMultiple($this->settings['variables']);
+                }
+
+                $configurationManager = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Configuration\ConfigurationManager::class);
+                $typoscriptSettings = $configurationManager->getConfiguration(
+                    ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
+                );
+                if (!empty($typoscriptSettings['lib.']['contentElement.']['partialRootPaths.'])) {
+                    $paths = [];
+                    foreach ($typoscriptSettings['lib.']['contentElement.']['partialRootPaths.'] as $tempPath) {
+                        $paths[] = $tempPath;
+                    }
+
+                    if (!empty($this->settings['additionalPartialPaths'])) {
+                        foreach (GeneralUtility::trimExplode(',', $this->settings['additionalPartialPaths']) as $tempPath) {
+                            $folder = $this->resourceFactory->getFolderObjectFromCombinedIdentifier($tempPath);
+                            $readablePath = $folder->getReadablePath();
+                            $storage = $folder->getStorage();
+                            $basePath = $storage->getConfiguration()['basePath'];
+                            $newTempPath = $basePath . ltrim( $readablePath, '/' );
+                            $paths[] = $newTempPath;
+                        }
+                    }
+
+                    $view->setPartialRootPaths($paths);
+                }
 
 
                 $this->view->assign('content', $view->render());
