f1c293cba97545a3eff851e8fb4f42a15c2a9860
Martin Pribyl <martin.pribyl@hyperdigital.de>
2025-11-04 14:12:58 +0100

    Integration of styleguide

---
+++ ext_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `pages`
+(
+	hd_dev_styleguide varchar(255) DEFAULT '' NOT NULL,
+);
+CREATE TABLE `tt_content`
+(
+	hd_dev_styleguide varchar(255) DEFAULT '' NOT NULL,
+);
+CREATE TABLE `sys_file`
+(
+	hd_dev_styleguide varchar(255) DEFAULT '' NOT NULL,
+);
